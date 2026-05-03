e0d9814402c9a9922478ab0218f28513ab4e2a6e
ssatyendra081-debug <ssatyendra081@gmail.com>
2026-05-03 12:55:55 +0530

    infracode

---
+++ module/azurerm_compute/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_linux_virtual_machine" "linvm" {
+  for_each            = var.vms
+  name                = each.value.vm_name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  size                = each.value.vm_size
+  admin_username      = "azureuser"
+  admin_password      = "Password@1234"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    data.azurerm_network_interface.nic[each.key].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
